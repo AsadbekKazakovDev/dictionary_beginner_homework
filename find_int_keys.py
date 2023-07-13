f15682891715e202c1d15252f17de7934c13e6b2
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-13 11:14:26 +0500

    solve main

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,4 +6,14 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    return 
+    lst=[]
+    for i in data.keys():
+        if type(i)==int:
+            lst.append(i)
+    return lst
+data = {
+    'a': 1, 
+    3 : 2, 
+    'c': 3,
+    10:'a'}
+print(find_int_keys(data))
